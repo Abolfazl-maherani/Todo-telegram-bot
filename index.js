4c0c48713d85dd553b546c48dfc6a218db74bf1b
Abolfazl maheranit <mahrani.prj@gmail.com>
2023-07-23 20:25:25 +0330

    Test bot

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.launch()
+bot.command('start', (ctx) => {
+    ctx.reply('شروع ربات...')
+
+})
+bot.command('ctx', (ctx) => {
+    const {chat, from, message, botInfo} = ctx
+    const json = JSON.stringify({chat, from, message, botInfo}, null, 4)
+    ctx.reply(json)
+    console.log(json)
+})
+bot.hears('بی ادب', (ctx)=>{
+    ctx.deleteMessage().then(res=>{
+        ctx.reply('خودت بی ادبی')
+    })
+
+})
+
+bot.launch().then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log('error Bot', err)
+})
+
+
